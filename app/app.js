2f0f54066a5a0b9994a8696f13aa4409d6646717
Vinay <me@vinaykumar.co>
2019-05-23 15:22:13 +0530

    Add contacts module

--- app/app.js
+++ app/app.js
@@ -5,8 +5,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+
+// Contacts
+app.use('/api', require('./routes/contacts'));
+
 
 module.exports = app;
