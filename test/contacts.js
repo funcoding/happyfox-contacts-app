48e0ef55931b33949cf19e77490b0e905f4400ef
Vinay <me@vinaykumar.co>
2019-05-25 11:42:24 +0530

    Updated test cases

--- test/contacts.js
+++ test/contacts.js
@@ -96,6 +96,112 @@ describe('POST /contacts', function () {
   });
 });
 
+describe('GET /contacts/:id', function () {
+  it('Creates new contact and fetch by id', async function () {
+    const contact = { name: 'John Doe', email: [{ content: 'johndoe@gmail.com', tag: 'work' }] };
+    let response = await request(app).post('/api/contacts')
+      .send(contact)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    const { id } = response.body.data;
+    response = await request(app)
+      .get(`/api/contacts/${id}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    return assert(response.body.data.name === contact.name, 'Contact name should match with John Doe');
+  });
+
+  it('Throws 422 when provided id is non mongoose id', async function () {
+    return request(app)
+      .get(`/api/contacts/123`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(422);
+  });
+
+  it('Throws 404 with non existence resource id', async function () {
+    return request(app)
+      .get(`/api/contacts/${mongoose.Types.ObjectId}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404);
+  });
+});
+
+describe('DELETE /contacts/:id', function () {
+  it('Creates new contact and delete by id', async function () {
+    const contact = { name: 'John Doe', email: [{ content: 'johndoe@gmail.com', tag: 'work' }] };
+    let response = await request(app).post('/api/contacts')
+      .send(contact)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    const { id } = response.body.data;
+    return request(app)
+      .delete(`/api/contacts/${id}`)
+      .set('Accept', 'application/json')
+      .expect(204);
+  });
+
+  it('Throws 422 when provided id is non mongoose id', async function () {
+    return request(app)
+      .delete(`/api/contacts/123`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(422);
+  });
+
+  it('Throws 404 with non existence resource id', async function () {
+    return request(app)
+      .delete(`/api/contacts/${mongoose.Types.ObjectId}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404);
+  });
+});
+
+describe('Creates new contact and search contact by wither name or email or phone', function () {
+  const contact = { name: 'Jane Doe', email: [{ content: 'janedoe@gmail.com', tag: 'work' }], phone: [{ content: '00000000', tag: 'work' }] };
+  it('Create new contact', async function () {
+    await request(app).post('/api/contacts')
+      .send(contact)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+  });
+
+  it('search by name', async function () {
+    const response = await request(app)
+      .get(`/api/contacts?search=${contact.name}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    assert(response.body.data.meta.total === 1, 'Total records should be one');
+    assert(response.body.data.records[0].name === contact.name, 'Contact name should match with Jane Doe');
+  });
+
+  it('search by email', async function () {
+    const response = await request(app)
+      .get(`/api/contacts?search=${contact.email[0].content}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    assert(response.body.data.meta.total === 1, 'Total records should be one');
+  });
+
+  it('search by phone', async function () {
+    const response = await request(app)
+      .get(`/api/contacts?search=${contact.phone[0].content}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    assert(response.body.data.meta.total === 1, 'Total records should be one');
+  });
+});
+
+
 after(async function (done) {
   try {
     if (server) {
